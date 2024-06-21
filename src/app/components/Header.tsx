5741ad376e78eee176a6b6e1efe3e8cf8f965590
mtakada <mori.takada@elife.co.jp>
2024-06-21 16:31:47 +0900

    add header logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
     <header className="ucul-header">
-      <h1>eLife Auto Detection System</h1>
+      <h1>
+        <Image src="/img/logo.png" alt="eLife" width={40} height={45} className="inline mr-2" />
+        Auto Detection System
+      </h1>
     </header>
   );
 }
